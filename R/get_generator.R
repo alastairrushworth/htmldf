c99dd8d9e2351ebc498373de7d59733938fcb378
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2020-04-11 10:11:43 +0100

    look for pkgdown in comments

--- R/get_generator.R
+++ R/get_generator.R
@@ -10,7 +10,17 @@ get_generator <- function(page, show_progress = TRUE){
         rvest::html_node('meta[name="generator"]') %>%
         rvest::html_attr('content'), silent = TRUE)
       if(!'try-error' %in% class(html_gen)){
-        generator[i] <- html_gen
+        if(is.na(html_gen)){
+          # check comments for pkgdown
+          generator[i] <- page[[i]] %>%
+            html_nodes(xpath=".//comment()") %>%
+            grepl(' pkgdown ', x = .) %>%
+            any() %>%
+            ifelse(., 'pkgdown', NA)
+        } else{
+          generator[i] <- html_gen
+        }
+
       } else {
         generator[i] <- NA
       }
